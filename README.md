6f9c0c0734f6c9076375e6981104438fc9699bff
shift-runstop <shift-runstop@protonmail.com>
2022-09-15 14:34:45 +0100

    running edit as required use dictates

---
+++ README.md
@@ -0,0 +1,10 @@
+# Quick and dirty webscreaper
+
+Revision required, as the name suggests it's quick and dirty.
+
+run with:
+
+```sh
+$ node index.js
+
+```
--- index.js
+++ index.js
@@ -4,4 +4,23 @@ const cheerio = require("cheerio")
 
 const app = express()
 const PORT = 1453
+const url = ''
 app.listen(PORT, () => console.log(`server runs on ${PORT}`))
+
+axios(url)
+	.then(response => {
+		const html = response.data
+		const $ = cheerio.load(html)
+		const links = []
+
+		$('.', html).each(function() {
+			const title = $(this).text()
+			const url = $(this).find('a').attr('href')
+			links.push({
+				title,
+				url
+			})
+		})
+
+		console.log(links)
+	}).catch(err => console.log(err))
